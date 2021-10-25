1f1c2de33b72b2f6e99ff6eb703a38bc74279f17
Anirudh Kanabar <anirudhkanabar@gmail.com>
2021-10-25 14:35:27 +0530

    Tutorial 3 complete

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'Anirudh Kanabar',
+        'title': 'Blog Post 1',
+        'content': 'First Post content',
+        'date_posted': 'October 25, 2021'
+    },
+    {
+        'author': 'Anirudh Kanabar',
+        'title': 'Blog Post 2',
+        'content': 'Second Post content',
+        'date_posted': 'October 25, 2021'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title':'About'})
